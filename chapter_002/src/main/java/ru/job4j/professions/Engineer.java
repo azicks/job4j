6c23ada767d7e4baddbeaf09701d5e785465471f
azicks <emperon@rambler.ru>
2018-12-09 17:00:42 +0700

    1. Реализация профессий в коде[#92684]

---
+++ chapter_002/src/main/java/ru/job4j/professions/Engineer.java
@@ -0,0 +1,7 @@
+package ru.job4j.professions;
+
+public class Engineer {
+    public void build(House h) {
+
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/professions/Profession.java
@@ -0,0 +1,9 @@
+package ru.job4j.professions;
+
+/**
+ * @since 09.12.2018
+ */
+public class Profession {
+    public String profession;
+    public String name;
+}
