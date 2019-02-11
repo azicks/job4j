32d57624d4e00c3b2b6c5859e8e9ccba3ed3e2e4
azicks <emperon@rambler.ru>
2019-02-11 15:03:19 +0700

    pom.xml

--- chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
+++ chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -6,17 +6,18 @@ import java.util.LinkedList;
 public class PriorityQueue {
     private LinkedList<Task> tasks = new LinkedList<>(new ArrayList<Task>(5));
 
- /**
+    /**
      * Метод должен вставлять в нужную позицию элемент.
      * Позиция определять по полю приоритет.
      * Для вставик использовать add(int index, E value)
+     *
      * @param task задача
      */
     public void put(Task task) {
         while (this.tasks.size() < task.getPriority()) {
             this.tasks.add(null);
         }
-        tasks.add(task.getPriority(),task);
+        tasks.add(task.getPriority(), task);
     }
 
     public Task take() {
