b990198a557bd44f46a0381181764af01fe1a2af
azicks <emperon@rambler.ru>
2019-04-21 12:44:32 +0700

    2.001.1.1 Итератор для двухмерного массива int[][][#134107]

---
+++ p2_001_collections_pro/src/main/java/ru/job4j/iterator/JaggedArrayIterator.java
@@ -0,0 +1,36 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+
+public class JaggedArrayIterator implements Iterator {
+    private int[][] matrix;
+    private int index = 0;
+    private int length = 0;
+    private int majorIndex = 0;
+    private int minorIndex = 0;
+
+    public JaggedArrayIterator(int[][] matrix) {
+        this.matrix = matrix;
+        for(int i = 0; i != matrix.length; i++) {
+            this.length += matrix[i].length;
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index < length;
+    }
+
+    @Override
+    public Object next() {
+        int result = matrix[majorIndex][minorIndex];
+        if (hasNext()) {
+            if (++minorIndex == matrix[majorIndex].length) {
+                majorIndex++;
+                minorIndex = 0;
+            }
+        }
+        index ++;
+        return result;
+    }
+}
