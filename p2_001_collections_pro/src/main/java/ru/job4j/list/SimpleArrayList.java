7302b18b6668e489c2599f27d8ccd92e40b2e795
azicks <emperon@rambler.ru>
2019-06-20 14:43:49 +0700

    2.001.3.0 Создать метод delete для односвязного списка[#134096]

---
+++ p2_001_collections_pro/src/main/java/ru/job4j/list/SimpleArrayList.java
@@ -0,0 +1,67 @@
+package ru.job4j.list;
+
+public class SimpleArrayList<E> {
+    private int size;
+    private Node<E> first;
+
+    /**
+     * Метод вставляет в начало списка данные.
+     */
+    public void add(E data) {
+        Node<E> newLink = new Node<>(data);
+        newLink.next = this.first;
+        this.first = newLink;
+        this.size++;
+    }
+
+    /**
+     * Реализовать метод удаления первого элемент в списке.
+     */
+    public E delete(int index) {
+        Node<E> result = this.first;
+        Node<E> prev;
+        if (index < 1) {
+            this.first = result.next;
+        } else {
+            prev = this.first;
+            int i;
+            for (i = 0; i < index - 1; i++) {
+                prev = prev.next;
+            }
+            result = prev.next;
+            prev.next = result.next;
+        }
+        return result.data;
+    }
+
+    /**
+     * Метод получения элемента по индексу.
+     */
+    public E get(int index) {
+        Node<E> result = this.first;
+        for (int i = 0; i < index; i++) {
+            result = result.next;
+        }
+        return result == null ? null : result.data;
+    }
+
+    /**
+     * Метод получения размера коллекции.
+     */
+    public int getSize() {
+        return this.size;
+    }
+
+    /**
+     * Класс предназначен для хранения данных.
+     */
+    private static class Node<E> {
+
+        E data;
+        Node<E> next;
+
+        Node(E data) {
+            this.data = data;
+        }
+    }
+}
---
+++ p2_001_collections_pro/src/test/java/ru/job4j/list/SimpleArrayListTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.list;
+
+import org.junit.Test;
+import org.junit.Before;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
+public class SimpleArrayListTest {
+
+    private SimpleArrayList<Integer> list;
+
+    @Before
+    public void beforeTest() {
+        list = new SimpleArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+    }
+
+    @Test
+    public void whenAddThreeElementsThenUseGetOneResultTwo() {
+        assertThat(list.get(1), is(2));
+    }
+
+    @Test
+    public void whenAddThreeElementsThenUseGetTwoResultOne() {
+        assertThat(list.get(2), is(1));
+    }
+
+    @Test
+    public void whenAddThreeElementsThenUseGetSizeResultThree() {
+        assertThat(list.getSize(), is(3));
+    }
+
+    @Test
+    public void whenDeleteFirstElementThenTwoThree() {
+        assertThat(list.delete(0), is(3));
+        assertThat(list.get(0), is(2));
+        assertThat(list.get(1), is(1));
+        assertNull(list.get(2));
+    }
+
+    @Test
+    public void whenDeleteSecondElementThenOneThree() {
+        assertThat(list.delete(1), is(2));
+        assertThat(list.get(0), is(3));
+        assertThat(list.get(1), is(1));
+        assertNull(list.get(2));
+    }
+}
