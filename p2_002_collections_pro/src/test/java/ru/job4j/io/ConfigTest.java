bf87b8572baab5aa8ef716d670bc5b17c12f3704
azicks <emperon@rambler.ru>
2019-10-28 18:34:37 +0700

    2.002.1.1. Читаем файл конфигурации[#134135]

---
+++ p2_002_collections_pro/src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/app.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(
+                config.value("hibernate.connection.username"),
+                is("postgres")
+        );
+        assertNull(config.value("when no value"));
+    }
+}
