13bad1b28f89ba6575ea88ad115dc4754215661f
azicks <emperon@rambler.ru>
2020-01-27 12:50:43 +0700

    2.003.2 Фильтры[#134080] fix

--- p2_003_SQL/data/query.sql
+++ p2_003_SQL/data/query.sql
@@ -16,26 +16,27 @@ insert into product (name, type_id, expired_date, price) values
 					
 select * from product;
 --------------------------------------------------------------
--- 1.
+-- 1. Написать запрос получение всех продуктов с типом "СЫР"
 select * from product where type_id = (select id from type where name = 'СЫР');
 
--- 2.
+-- 2. Написать запрос получения всех продуктов, у кого в имени есть слово "мороженное"
 select * from product where name like 'Мороженое';
 
--- 3.
+-- 3. Написать запрос, который выводит все продукты, срок годности которых заканчивается в следующем месяце.
 select * from product where extract(month from expired_date) = extract(month from now()) + 1;
 
--- 4.
+-- 4. Написать запрос, который выводит самый дорогой продукт.
 select * from product order by price desc limit 1;
 
--- 5.
-select * from product where type_id = (select id from type where name = 'СЫР') or 
-							type_id = (select id from type where name = 'МОЛОКО');
--- 6.
--- Не понял, в таблице нет поля количества продуктов, напишу запрос на вывод типа продуктов дешевле 100
-select t.name from product as p
-inner join type as t on p.type_id = t.id and p.price < '100';
+-- 5.  Написать запрос, который выводит количество всех продуктов определенного типа.
+select count(id) from product where type_id = (select id from type where name = 'СЫР') ; 
 
--- 7.
+-- 6. Написать запрос получение всех продуктов с типом "СЫР" и "МОЛОКО"
+select * from product where type_id in (select id from type where name in ('СЫР', 'МОЛОКО'));
+
+-- 7. Написать запрос, который выводит тип продуктов, которых осталось меньше 10 штук.  
+select name from type where id in (select type_id from product group by type_id having(count(*) < 10));
+
+-- 8. Вывести все продукты и их тип.
 select * from product as p 
 full join type as t on t.id = p.type_id;
